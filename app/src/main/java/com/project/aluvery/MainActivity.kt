761698aa8ac0a988b4c8cf295a402cfbe3a00ebe
Livia Carvalho <livia.carvalho.lb@gmail.com>
2024-06-25 14:34:14 -0400

    feat: created ProductItem component

--- app/src/main/java/com/project/aluvery/MainActivity.kt
+++ app/src/main/java/com/project/aluvery/MainActivity.kt
@@ -3,44 +3,112 @@ package com.project.aluvery
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.foundation.Image
 import androidx.compose.material3.Surface
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.widthIn
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.project.aluvery.ui.theme.AluveryTheme
+import com.project.aluvery.ui.theme.Purple200
+import com.project.aluvery.ui.theme.Teal200
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             AluveryTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+                ProductItem()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun ProductItem(
+    modifier: Modifier = Modifier
+) {
+
+    Surface(shape = RoundedCornerShape(15.dp), shadowElevation = 4.dp) {
+        Column(
+            modifier
+                .width(200.dp)
+                .heightIn(min = 250.dp, max = 300.dp)
+        ) {
+            val imageSize = 100.dp
+            Box(
+                modifier
+                    .background(
+                        brush = Brush.horizontalGradient(
+                            colors = listOf(
+                                Purple200, Teal200
+                            )
+                        )
+                    )
+                    .fillMaxWidth()
+                    .height(100.dp)
+            ) {
+                Image(
+                    painterResource(id = R.drawable.ic_launcher_background),
+                    contentDescription = "Product Image",
+                    modifier = Modifier
+                        .size(imageSize)
+                        .offset(y = imageSize / 2)
+                        .clip(CircleShape)
+                        .align(Alignment.BottomCenter),
+                    contentScale = ContentScale.Crop
+                )
+
+            }
+            Spacer(Modifier.height(imageSize/2))
+            Column(Modifier.padding(16.dp)) {
+                Text(
+                    text = "Texto de descrição do produto disponibilizado",
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight(700),
+                    maxLines = 2,
+                    overflow = TextOverflow.Ellipsis
+                )
+                Text(
+                    text = "R$ 19,99",
+                    modifier = Modifier.padding(top = 8.dp),
+                    fontSize = 14.sp,
+                    fontWeight = FontWeight(400)
+
+                )
+            }
+        }
+    }
+
 }
+
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    AluveryTheme {
-        Greeting("Android")
-    }
+fun ProductItemPreview() {
+    ProductItem()
 }
